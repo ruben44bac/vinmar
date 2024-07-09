9ae5260eb786932124568d5e3c3e5520e3e6ac8f
ruben44bac <ruben44bac@gmail.com>
2024-07-09 09:40:22 -0600

    Added example charts

--- lib/vinmar_web/live/home/index.ex
+++ lib/vinmar_web/live/home/index.ex
@@ -2,6 +2,18 @@ defmodule VinmarWeb.HomeLive.Index do
   use VinmarWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    send(self(), :after_join)
+    {:ok, assign(socket, show_table: false)}
   end
+
+  def handle_info(:after_join, socket) do
+    Task.async(fn -> :show_table end)
+    {:noreply, socket}
+  end
+
+  def handle_info({_ref, :show_table}, socket) do
+    {:noreply, assign(socket, show_table: true)}
+  end
+
+  def handle_info({:DOWN, _, :process, _, :normal}, socket), do: {:noreply, socket}
 end
